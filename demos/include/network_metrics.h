e0ff5c222155b2dda241193d0b8c9d3d45b2b4a4
Gaurav Aggarwal <aggarg@amazon.com>
2020-11-06 00:23:29 +0000

    Metrics collector suggestions
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- demos/include/network_metrics.h
+++ demos/include/network_metrics.h
@@ -1,41 +1,25 @@
-#ifndef __METRICS_H__
-#define __METRICS_H__
+#ifndef METRICS_COLLECTOR_H_
+#define METRICS_COLLECTOR_H_
 
 /* Standard includes. */
 #include <stdint.h>
-/* Lwip includes */
-#include "lwipopts.h"
-#include "lwip/ip_addr.h"
-
-
-#if !defined( FREERTOS_LWIP_METRICS_ENABLE ) || ( FREERTOS_LWIP_METRICS_ENABLE == 0 )
-    #warning "FREERTOS_LWIP_METRICS_ENABLE is disabled."
-#endif
-
-#if !defined( LWIP_TCPIP_CORE_LOCKING_INPUT ) || ( LWIP_TCPIP_CORE_LOCKING_INPUT == 0 )
-    #warning "The network metric will not be supported if LWIP_TCPIP_CORE_LOCKING_INPUT is disabled."
-#endif
-
-
-/* Extern veriables from LWIP */
-extern struct netif sta_if, ap_if;
 
 /**
  * @brief Return codes from metrics collector APIs.
  */
 typedef enum
 {
-    LwipMetricSuccess = 0,
-    LwipMetricBadParameter
-} LwipMetricStatus_t;
+    MetricSuccess = 0,
+    MetricBadParameter
+} MetricStatus_t;
 
 /**
  * @brief Represents a network connection.
  */
 typedef struct Connection
 {
-    ip_addr_t localIp;
-    ip_addr_t remoteIp;
+    uint32_t localIp;
+    uint32_t remoteIp;
     uint16_t localPort;
     uint16_t remotePort;
 } Connection_t;
@@ -47,26 +31,22 @@ typedef struct NetworkStats
 {
     uint32_t bytesReceived;   /**< Number of bytes received. */
     uint32_t bytesSent;       /**< Number of bytes sent. */
-    uint32_t packetsReceived; /**< Number of TCP packets received. */
-    uint32_t packetsSent;     /**< Number of TCP packets sent. */
+    uint32_t packetsReceived; /**< Number of packets (ethernet frames) received. */
+    uint32_t packetsSent;     /**< Number of packets (ethernet frames) sent. */
 } NetworkStats_t;
 
+MetricStatus_t GetNetworkStats( NetworkStats_t * pOutNetworkStats );
 
-#define LWIP_GET_TCP_PACKET_IN()           ( lwip_stats.mib2.ipinreceives )
-#define LWIP_GET_TCP_PACKET_OUT()          ( lwip_stats.mib2.ipoutrequests + lwip_stats.mib2.ipforwdatagrams ) /*need review */
+MetricStatus_t GetEstablishedConnections( Connection_t * pOutConnectionsArray,
+                                          uint32_t connectionsArrayLength,
+                                          uint32_t * pOutNumEstablishedConnections );
 
-#define LWIP_GET_NETIF_PACKET_IN_sta()     ( sta_if.mib2_counters.ifinoctets )
-#define LWIP_GET_NETIF_PACKET_OUT_sta()    ( sta_if.mib2_counters.ifoutoctets )
+MetricStatus_t GetOpenTcpPorts( uint16_t * pOutTcpPortsArray,
+                                uint32_t tcpPortsArrayLength,
+                                uint32_t * pOutNumOpenTcpPorts );
 
-#define LWIP_GET_NETIF_PACKET_IN_ap()      ( ap_if.mib2_counters.ifinoctets )
-#define LWIP_GET_NETIF_PACKET_OUT_ap()     ( ap_if.mib2_counters.ifoutoctets )
+MetricStatus_t GetOpenUdpPorts( uint16_t * pOutUdpPortsArray,
+                                uint32_t udpPortsArrayLength,
+                                uint32_t * pOutNumOpenTcpPorts );
 
-extern LwipMetricStatus_t lwipGetNetworkStats( NetworkStats_t * pOutNetworkStats );
-extern LwipMetricStatus_t lwipGetEstablishedConnections( Connection_t * pOutConnectionsArray,
-                                                         uint32_t connectionsArrayLength,
-                                                         uint32_t * pOutNumEstablishedConnections );
-extern LwipMetricStatus_t lwipGetOpenTcpPorts( uint16_t * pOutPortsArray,
-                                               uint32_t portsArrayLength,
-                                               uint32_t * pOutNumOpenPorts );
-
-#endif //__METRICS_H__
+#endif /* METRICS_COLLECTOR_H_ */
--- demos/network_metrics/network_metrics.c
+++ demos/network_metrics/network_metrics.c
@@ -1,23 +1,35 @@
-/* Standard includes. */
-#include <stdint.h>
-/* Lwip includes. */
-#include "lwip/arch.h"
-#include "lwip/stats.h"
-#include "lwip/tcpip.h"         /* #define LOCK_TCPIP_CORE()     sys_mutex_lock(&lock_tcpip_core) */
-#include "lwip/ip_addr.h"       /*ip_addr_t, ipaddr_ntoa, ip_addr_copy */
-#include "lwip/tcp.h"           /* struct tcp_pcb */
-#include "lwip/priv/tcp_priv.h" /*tcp_listen_pcbs_t */
-/* Platform layer includes. */
-#include "platform/iot_threads.h"
-/* Demo logging include. */
-#include "iot_demo_logging.h"
-/* Interface includes. */
+/* Interface include. */
 #include "network_metrics.h"
 
-#if ( FREERTOS_LWIP_METRICS_ENABLE == 1 )
+/* Lwip includes. */
+#include "lwipopts.h"
+#include "lwip/ip_addr.h"
+#include "lwip/arch.h"
+#include "lwip/stats.h"
+#include "lwip/tcpip.h"
+#include "lwip/ip_addr.h"
+#include "lwip/tcp.h"
+#include "lwip/priv/tcp_priv.h"
 
-    extern struct tcp_pcb * tcp_active_pcbs;        /* List of all TCP PCBs that are in a state in which they accept or send data. */
-    extern union tcp_listen_pcbs_t tcp_listen_pcbs; /* List of all TCP PCBs in LISTEN state */
+/* Demo logging include. */
+#include "iot_demo_logging.h" /* GA - Why don't we use the new logging framework? */
+
+#if !defined( LWIP_TCPIP_CORE_LOCKING_INPUT ) || ( LWIP_TCPIP_CORE_LOCKING_INPUT == 0 )
+    #error "The network metric will not be supported if LWIP_TCPIP_CORE_LOCKING_INPUT is disabled."
+#endif
+
+/* Extern variables from LWIP. */
+extern struct netif sta_if, ap_if;
+
+#define LWIP_GET_PACKET_IN()           ( lwip_stats.mib2.ipinreceives )
+#define LWIP_GET_PACKET_OUT()          ( lwip_stats.mib2.ipoutrequests )
+
+#define LWIP_GET_BYTES_IN()            ( sta_if.mib2_counters.ifinoctets )
+#define LWIP_GET_BYTES_OUT()           ( sta_if.mib2_counters.ifoutoctets )
+
+/* Defined in the LWIP code. */
+extern struct tcp_pcb * tcp_active_pcbs;        /* List of all TCP PCBs that are in a state in which they accept or send data. */
+extern union tcp_listen_pcbs_t tcp_listen_pcbs; /* List of all TCP PCBs in LISTEN state */
 
 /**
  * @brief Get a list of the listening TCP ports.
@@ -35,57 +47,57 @@
  * @return #LwipMetricSuccess if open TCP ports are successfully obtained;
  * #LwipMetricBadParameter if invalid parameters are passed;
  */
-    LwipMetricStatus_t lwipGetOpenTcpPorts( uint16_t * pOutTcpPortsArray,
-                                            uint32_t portsArrayLength,
-                                            uint32_t * pOutNumTcpOpenPorts )
+MetricStatus_t GetOpenTcpPorts( uint16_t * pOutTcpPortsArray,
+                                uint32_t tcpPortsArrayLength,
+                                uint32_t * pOutNumTcpOpenPorts )
+{
+    MetricStatus_t status = MetricSuccess;
+    struct tcp_pcb_listen * pCurrPcb = tcp_listen_pcbs.listen_pcbs;
+    uint16_t pcbCnt = 0;
+
+    if( ( ( pOutTcpPortsArray != NULL ) && ( tcpPortsArrayLength == 0 ) ) ||
+        ( pOutNumTcpOpenPorts == NULL ) )
     {
-        LwipMetricStatus_t status = LwipMetricSuccess;
-        struct tcp_pcb_listen * pCurrPcb = tcp_listen_pcbs.listen_pcbs;
-        uint16_t pcbCnt = 0;
-
-        if( ( ( pOutTcpPortsArray != NULL ) && ( portsArrayLength == 0 ) ) ||
-            ( pOutNumTcpOpenPorts == NULL ) )
-        {
-            IotLogError( "Invalid parameters. pOutTcpPortsArray: 0x%08x,"
-                         "portsArrayLength: %u, pOutNumTcpOpenPorts: 0x%08x.",
-                         pOutTcpPortsArray,
-                         portsArrayLength,
-                         pOutNumTcpOpenPorts );
-            status = LwipMetricBadParameter;
-        }
-
-        if( status == LwipMetricSuccess )
-        {
-            LOCK_TCPIP_CORE();
-
-            while( pCurrPcb )
-            {
-                /*  write the ports into pOutTcpPortsArray if not NULL */
-                if( ( pOutTcpPortsArray != NULL ) && ( pcbCnt < portsArrayLength ) )
-                {
-                    pOutTcpPortsArray[ pcbCnt ] = pCurrPcb->local_port;
-                }
-
-                ++pcbCnt;
-                pCurrPcb = pCurrPcb->next;
-            }
-
-            UNLOCK_TCPIP_CORE();
-        }
-
-        if( ( pOutTcpPortsArray != NULL ) && ( pcbCnt > portsArrayLength ) )
-        {
-            IotLogWarn( "The portsArrayLength is not long engough to store all the listening ports" );
-        }
-
-        if( status == LwipMetricSuccess )
-        {
-            *pOutNumTcpOpenPorts = pcbCnt;
-        }
-
-        return status;
+        IotLogError( "Invalid parameters. pOutTcpPortsArray: 0x%08x,"
+                        "portsArrayLength: %u, pOutNumTcpOpenPorts: 0x%08x.",
+                        pOutTcpPortsArray,
+                        portsArrayLength,
+                        pOutNumTcpOpenPorts );
+        status = MetricBadParameter;
     }
 
+    if( status == MetricSuccess )
+    {
+        LOCK_TCPIP_CORE();
+
+        while( pCurrPcb )
+        {
+            /*  write the ports into pOutTcpPortsArray if not NULL. */
+            if( ( pOutTcpPortsArray != NULL ) && ( pcbCnt < tcpPortsArrayLength ) )
+            {
+                pOutTcpPortsArray[ pcbCnt ] = pCurrPcb->local_port;
+            }
+
+            ++pcbCnt;
+            pCurrPcb = pCurrPcb->next;
+        }
+
+        UNLOCK_TCPIP_CORE();
+    }
+
+    if( ( pOutTcpPortsArray != NULL ) && ( pcbCnt > tcpPortsArrayLength ) )
+    {
+        IotLogWarn( "The portsArrayLength is not long engough to store all the listening ports" );
+    }
+
+    if( status == MetricSuccess )
+    {
+        *pOutNumTcpOpenPorts = pcbCnt;
+    }
+
+    return status;
+}
+/*-----------------------------------------------------------*/
 
 /**
  * @brief Get a list of established connections.
@@ -105,63 +117,64 @@
  * @return #LwipMetricSuccess if open TCP ports are successfully obtained;
  * #LwipMetricBadParameter if invalid parameters are passed;
  */
-    LwipMetricStatus_t lwipGetEstablishedConnections( Connection_t * pOutConnectionsArray,
-                                                      uint32_t connectionsArrayLength,
-                                                      uint32_t * pOutNumEstablishedConnections )
+MetricStatus_t GetEstablishedConnections( Connection_t * pOutConnectionsArray,
+                                          uint32_t connectionsArrayLength,
+                                          uint32_t * pOutNumEstablishedConnections )
+{
+    MetricStatus_t status = MetricSuccess;
+    struct tcp_pcb * pCurrPcb = tcp_active_pcbs;
+    uint16_t pcbCnt = 0;
+    Connection_t * pEstablishedConnection;
+
+    if( ( ( pOutConnectionsArray != NULL ) && ( connectionsArrayLength == 0 ) ) ||
+        ( pOutNumEstablishedConnections == NULL ) )
     {
-        LwipMetricStatus_t status = LwipMetricSuccess;
-        struct tcp_pcb * pCurrPcb = tcp_active_pcbs;
-        uint16_t pcbCnt = 0;
-        Connection_t * pEstablishedConnection;
+        IotLogError( "Invalid parameters. pOutConnectionsArray: 0x%08x,"
+                        " connectionsArrayLength: %u, pOutNumEstablishedConnections: 0x%08x.",
+                        pOutConnectionsArray,
+                        connectionsArrayLength,
+                        pOutNumEstablishedConnections );
+        status = MetricBadParameter;
+    }
 
-        if( ( ( pOutConnectionsArray != NULL ) && ( connectionsArrayLength == 0 ) ) ||
-            ( pOutNumEstablishedConnections == NULL ) )
+    if( status == MetricSuccess )
+    {
+        LOCK_TCPIP_CORE();
+
+        while( pCurrPcb )
         {
-            IotLogError( "Invalid parameters. pOutConnectionsArray: 0x%08x,"
-                         " connectionsArrayLength: %u, pOutNumEstablishedConnections: 0x%08x.",
-                         pOutConnectionsArray,
-                         connectionsArrayLength,
-                         pOutNumEstablishedConnections );
-            status = LwipMetricBadParameter;
-        }
-
-        if( status == LwipMetricSuccess )
-        {
-            LOCK_TCPIP_CORE();
-
-            while( pCurrPcb )
+            /*  write the ports into pOutConnectionsArray if not NULL */
+            if( ( pOutConnectionsArray != NULL ) && ( pcbCnt < connectionsArrayLength ) )
             {
-                /*  write the ports into pOutConnectionsArray if not NULL */
-                if( ( pOutConnectionsArray != NULL ) && ( pcbCnt < connectionsArrayLength ) )
-                {
-                    /* The output array member to fill. */
-                    pEstablishedConnection = &( pOutConnectionsArray[ pcbCnt ] );
+                /* The output array member to fill. */
+                pEstablishedConnection = &( pOutConnectionsArray[ pcbCnt ] );
 
-                    ip_addr_copy( pEstablishedConnection->remoteIp, pCurrPcb->remote_ip ); /* network byte order */
-                    ip_addr_copy( pEstablishedConnection->localIp, pCurrPcb->local_ip );   /* network byte order */
-                    pEstablishedConnection->localPort = pCurrPcb->local_port;              /* host byte order */
-                    pEstablishedConnection->remotePort = pCurrPcb->remote_port;            /* host byte order */
-                }
-
-                ++pcbCnt;
-                pCurrPcb = pCurrPcb->next;
+                ip_addr_copy( pEstablishedConnection->remoteIp, pCurrPcb->remote_ip ); /* network byte order */
+                ip_addr_copy( pEstablishedConnection->localIp, pCurrPcb->local_ip );   /* network byte order */
+                pEstablishedConnection->localPort = pCurrPcb->local_port;              /* host byte order */
+                pEstablishedConnection->remotePort = pCurrPcb->remote_port;            /* host byte order */
             }
 
-            UNLOCK_TCPIP_CORE();
+            ++pcbCnt;
+            pCurrPcb = pCurrPcb->next;
         }
 
-        if( ( pOutConnectionsArray != NULL ) && ( pcbCnt > connectionsArrayLength ) )
-        {
-            IotLogWarn( "The connectionsArrayLength is not long engough to store all the established connections" );
-        }
-
-        if( status == LwipMetricSuccess )
-        {
-            *pOutNumEstablishedConnections = pcbCnt;
-        }
-
-        return status;
+        UNLOCK_TCPIP_CORE();
     }
+
+    if( ( pOutConnectionsArray != NULL ) && ( pcbCnt > connectionsArrayLength ) )
+    {
+        IotLogWarn( "The connectionsArrayLength is not long enough to store all the established connections" );
+    }
+
+    if( status == MetricSuccess )
+    {
+        *pOutNumEstablishedConnections = pcbCnt;
+    }
+
+    return status;
+}
+/*-----------------------------------------------------------*/
 
 /**
  * @brief Get network stats.
@@ -173,27 +186,26 @@
  * @return #LwipMetricSuccess if the network stats are successfully obtained;
  * #LwipMetricBadParameter if invalid parameters are passed;
  */
-    LwipMetricStatus_t lwipGetNetworkStats( NetworkStats_t * pOutNetworkStats )
+MetricStatus_t GetNetworkStats( NetworkStats_t * pOutNetworkStats )
+{
+    MetricStatus_t status = MetricSuccess;
+
+    if( pOutNetworkStats == NULL )
     {
-        LwipMetricStatus_t status = LwipMetricSuccess;
-
-        if( pOutNetworkStats == NULL )
-        {
-            IotLogError( "Invalid parameters. pOutNetworkStats: 0x%08x", pOutNetworkStats );
-            status = LwipMetricBadParameter;
-        }
-
-        if( status == LwipMetricSuccess )
-        {
-            LOCK_TCPIP_CORE();
-            pOutNetworkStats->bytesReceived = LWIP_GET_NETIF_PACKET_IN_sta();
-            pOutNetworkStats->bytesSent = LWIP_GET_NETIF_PACKET_OUT_sta();
-            pOutNetworkStats->packetsReceived = LWIP_GET_TCP_PACKET_IN();
-            pOutNetworkStats->packetsSent = LWIP_GET_TCP_PACKET_OUT();
-            UNLOCK_TCPIP_CORE();
-        }
-
-        return status;
+        IotLogError( "Invalid parameters. pOutNetworkStats: 0x%08x", pOutNetworkStats );
+        status = MetricBadParameter;
     }
 
-#endif //FREERTOS_LWIP_METRICS_ENABLE
+    if( status == MetricSuccess )
+    {
+        LOCK_TCPIP_CORE();
+        pOutNetworkStats->bytesReceived = LWIP_GET_BYTES_IN();
+        pOutNetworkStats->bytesSent = LWIP_GET_BYTES_OUT();
+        pOutNetworkStats->packetsReceived = LWIP_GET_PACKET_IN();
+        pOutNetworkStats->packetsSent = LWIP_GET_PACKET_OUT();
+        UNLOCK_TCPIP_CORE();
+    }
+
+    return status;
+}
+/*-----------------------------------------------------------*/
